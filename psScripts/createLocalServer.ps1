081a3615df58a147a2831bc618a2c2b84eaf36a7
Arun Sree Kumar <arun-sree-kumar@outlook.com>
2016-06-17 16:19:56 +0530

    Implemented dynamic html and method callling
    Implemented look up functionality for pages and get api and dynamic function method calling.

--- psScripts/createLocalServer.ps1
+++ psScripts/createLocalServer.ps1
@@ -19,25 +19,32 @@
 $path=$args[0]
 #----------------------------------------------------------------
 "Checking for configuration file at path " + $path 
-$configFilePath = [io.path]::combine($path,'app.config')  # using .NET Path class instead of join-path commandlet for consistency.
+$configFilePath = [io.path]::combine($path,'app.csv')  # using .NET Path class instead of join-path commandlet for consistency.
 #----------------------------------------------------------------
 "Reading Configuration File " + $configFilePath
 $appConfig = Import-Csv -Path $configFilePath
+#----------------------------------------------------------------
 $Pages=@{}
-$APIs = @{}
+$GET_APIs = @{}
 foreach($r in $appConfig)
 {
-    if ( $r.Type = 'Page')
+    if ( $r.Type -eq 'Page')
     {
-    $Pages[$r.URI]=$r.Location
+    $Pages[$r.URI + '[0]']=$r.Location
     }
-    if ( $r.Type = 'API')
+    elseif ( $r.Type -eq 'GET')
     {
-    $APIs[$r.URI]=$r.Location
+    $GET_APIs[$r.URI + '[' + $r.Params + ']'] = $r.Location
+    }
+    elseif ( $r.Type -eq 'ScriptFile' )
+    {
+       $scriptPath = [io.path]::combine($path,$r.Location)
+      . $scriptPath
     }
 }
 #----------------------------------------------------------------
-
+ShowSampleString
+" Pages - " + $Pages
 $listener = New-Object Net.HttpListener
 $listener.Prefixes.Add("http://localhost:8081/")
 "Starting the local server at port 8081"
@@ -45,18 +52,35 @@ $listener.Start()
 "Local server started and server listening to port 8081"
 
 "Opening the browser"
-Start-Process -FilePath "http://localhost:8081/home/"
+Start-Process -FilePath "http://localhost:8081/home"
 "Browser Opened with the url to local server"
 While ($listener.IsListening) {
     $context = $listener.GetContext()
     $request = $context.Request
     "Raw URL " + $request.RawUrl
+
     $scriptpath = $MyInvocation.MyCommand.Path
     $dir = Split-Path $scriptpath
     Write-host "My directory is $dir"
-    if ($request.RawUrl.ToLower().StartsWith("/parties/")) 
-    {     
-    $page = 'parties';    
+
+    $paramCount =  $request.Url.Segments.Count - 2
+    $thisPage = $Pages['/' + $request.Url.Segments[1] + '[0]']
+    $thisGETAPI = $GET_APIs['/' + $request.Url.Segments[1] + '[' + $paramCount + ']']
+
+    if($thisPage)
+    {
+        $pagePath = [io.path]::combine($path,$thisPage) 
+        $page = Get-Content -Path ($pagePath) -Raw
+    }
+    elseif ($thisGETAPI) 
+    {   
+        $args = "";  
+        for ($i=1; $i -le $paramCount; $i++)
+            {
+              $args = $args + " " + $request.Url.Segments[$i + 2]
+            } 
+        &($thisGETAPI + ' ' + $args)
+
     }
     elseif ($request.RawUrl.ToLower().StartsWith("/party"))
     {
