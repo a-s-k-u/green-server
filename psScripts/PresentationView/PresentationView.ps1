bcd3195f76613d128db1fb7f0ad9d72d73baf131
Arun Sree Kumar <arun-sree-kumar@outlook.com>
2016-07-05 16:51:42 +0530

    Update PresentationView.ps1

--- psScripts/PresentationView/PresentationView.ps1
+++ psScripts/PresentationView/PresentationView.ps1
@@ -1,4 +1,12 @@
-function ShowSampleString
+function getIdeas
 {
-Write-Host "hello... this is sample string"
+$dataPath = [io.path]::combine($global:rootFolder,'PresentationView\ADM1Digithonideas.csv')
+$a = Import-Csv -Path $dataPath | ConvertTo-Json
+return $a
+}
+function getSlides
+{
+$slidesPath = [io.path]::combine($global:rootFolder,'PresentationView\Slides.csv')
+$a = Import-Csv -Path $slidesPath | ConvertTo-Json
+return $a
 }
