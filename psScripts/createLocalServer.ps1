4e1586112520f043e7947d9fdbc529da0dc680cc
Arun Sree Kumar <arun-sree-kumar@outlook.com>
2016-08-19 14:32:19 +0530

    Remnoved the need for config file
    To make it easily manageable, realigned the code logic to rely more on the naming convention than on a configuration file.
    So, all get requests should have a corresponding method in PSScript.ps1 with a naming prefix get. eg: a get service url , /idea should have a getIdea method.
    Post methods should have a corresponding method in PSScript.ps1 file with a prefix post. eg: a post service url , /idea should have a postIdea in PSScript.ps1 file.

--- psScripts/createLocalServer.ps1
+++ psScripts/createLocalServer.ps1
@@ -16,45 +16,20 @@
  #*=============================================================================
 "Control reached powershell - setting up a local server/listener"
 #----------------------------------------------------------------
-$global:projectFolder = $args[0]
-'args - 0 is ' + $args[0]
+$global:projectFolder = 'C:\Users\i84446\Downloads\green-server-master\green-server-master\psScripts\PresentationView' # $args[0]
+'args - 0 is ' #+ $args[0]
 #----------------------------------------------------------------
 "Checking for configuration file at path " + $global:projectFolder
 $configFilePath = [io.path]::combine($global:projectFolder,'app.csv')  # using .NET Path class instead of join-path commandlet for consistency.
 #----------------------------------------------------------------
 "Reading Configuration File " + $configFilePath
-$appConfig = Import-Csv -Path $configFilePath
+#$appConfig = Import-Csv -Path $configFilePath
 #----------------------------------------------------------------
-$Pages=@{}
-$JS=@{}
-$CSS=@{}
-$GET_APIs = @{}
-foreach($r in $appConfig)
-{
-    if ( $r.Type -eq 'Page')
-    {
-    $Pages[$r.URI + '[0]']=$r.Location
-    }
-    if ( $r.Type -eq 'JS')
-    {
-    $JS[$r.URI + '[0]']=$r.Location
-    }
-    if ( $r.Type -eq 'CSS')
-    {
-    $CSS[$r.URI + '[0]']=$r.Location
-    }
-    elseif ( $r.Type -eq 'GET')
-    {
-    $GET_APIs[$r.URI + '[' + $r.Params + ']'] = $r.Location
-    }
-    elseif ( $r.Type -eq 'PSScriptFile' )
-    {
-       $scriptPath = [io.path]::combine($global:projectFolder,$r.Location)
-      . $scriptPath
-    }
-}
+"Including PS Script"
+$scriptPath = [io.path]::combine($global:projectFolder,'PSScript.ps1')
+. $scriptPath
 #----------------------------------------------------------------
-" Pages - " + $Pages.ToString();
+
 $listener = New-Object Net.HttpListener
 $listener.Prefixes.Add("http://localhost:8081/")
 "Starting the local server at port 8081"
@@ -62,7 +37,7 @@ $listener.Start()
 "Local server started and server listening to port 8081"
 
 "Opening the browser"
-Start-Process -FilePath "http://localhost:8081/home"
+Start-Process -FilePath "http://localhost:8081/index.html"
 "Browser Opened with the url to local server"
 
 try{
@@ -70,56 +45,51 @@ try{
     While ($listener.IsListening){
         $context = $listener.GetContext()
         $request = $context.Request
-        "Raw URL " + $request.RawUrl
+        $response = $context.Response
+        "Debug: Raw URL " + $request.RawUrl
 
+        <#
         $scriptpath = $MyInvocation.MyCommand.Path
         $dir = Split-Path $scriptpath
         Write-host "My directory is $dir"
+        #>
 
-        $paramCount =  $request.Url.Segments.Count - 2
-        $thisPage = $Pages['/' + $request.Url.Segments[1] + '[0]']
-        $thisGETAPI = $GET_APIs['/' + $request.Url.Segments[1] + '[' + $paramCount + ']']
-        $thisJSFile = $JS['/' + $request.Url.Segments[1] + '[0]']
-        $thisCSSFile = $CSS['/' + $request.Url.Segments[1] + '[0]']
-
-        'Debug : This Page is ' + $thisPage;
-        'Debug : This JS File is ' + $thisJSFile;
-
-         $response = $context.Response
-
-        if($thisPage)
-        {
-            $pagePath = [io.path]::combine($global:projectFolder,$thisPage) 
+        if ($request.RawUrl -Match ".html"){
+            $pagePath = [io.path]::combine($global:projectFolder,$request.RawUrl.TrimStart("/")) 
             $page = Get-Content -Path ($pagePath) -Raw
             $response.Headers.Add("Content-Type","text/html")
         }
-        elseif($thisJSFile)
-        {
-            $jsPath = [io.path]::combine($global:projectFolder,$thisJSFile) 
+        elseif($request.RawUrl -Match ".js"){
+            $jsPath = [io.path]::combine($global:projectFolder,$request.RawUrl.TrimStart("/")) 
             $page = Get-Content -Path ($jsPath) -Raw 
             $response.Headers.Add("Content-Type","application/javascript") 
         }
-        elseif($thisCSSFile)
-        {
-            $cssPath = [io.path]::combine($global:projectFolder,$thisCSSFile) 
+        elseif($request.RawUrl -Match ".css"){
+            $cssPath = [io.path]::combine($global:projectFolder,$request.RawUrl.TrimStart("/")) 
             $page = Get-Content -Path ($cssPath) -Raw 
             $response.Headers.Add("Content-Type","text/css") 
         }
-        elseif ($thisGETAPI) 
-        {   
+        elseif($request.HttpMethod.ToUpper() = 'GET'){
+            $paramCount =  $request.Url.Segments.Count - 2
+
             $args = "";  
             for ($i=1; $i -le $paramCount; $i++)
                 {
                   $args = $args + " " + $request.Url.Segments[$i + 2]
                 } 
             if($args -ne "") { $args = ' ' + $args } # addign a space seperator before callign the arguements
-            $page = &($thisGETAPI + $args)
+
+
+            $methodName = $request.HttpMethod + $request.Url.Segments[1]
+            $page = &($methodName + $args)
             $response.Headers.Add("Content-Type","application/json")
         }
-        else
-        {
-        $page = " incorrect URL"
-        $response.Headers.Add("Content-Type","text/html")     
+        elseif($request.HttpMethod.ToUpper() = 'POST'){
+         'POST METHOD ENCOUNTERED'
+        }
+        else{
+            $page = " incorrect URL"
+            $response.Headers.Add("Content-Type","text/html")     
         }
 
         $buffer = [System.Text.Encoding]::UTF8.GetBytes($page)
@@ -130,7 +100,10 @@ try{
     }
 
 } 
-finally {
+catch{
+'SOME EXCEPTION THROWN'
+}
+finally{
     $listener.Stop()
     $listener.Close()
 }
